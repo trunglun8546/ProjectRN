dff8f70093a6e01e67d45078d620df4d3ac0f3c7
123 <trung>
2020-05-19 17:03:19 +0700

    Simple

---
+++ FingerPrint.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+} from 'react-native';
+
+import Main from './src/components/Main';
+
+export default class FingerPrint extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Main />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F5FCFF',
+  },
+});
---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+import FingerPrint from './FingerPrint';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => FingerPrint);
